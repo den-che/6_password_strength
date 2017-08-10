990e872b5c4134ba0b015a0b02868e3023da4f0e
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-08-11 01:36:20 +0300

    First version password strength

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,24 @@
+import re
 
+def upper_case(password):
+    return re.search('[A-Z]', password)
+
+def lower_case(password):
+    return re.search('[a-z]',password)
+
+def check_special_symbols(password):
+    return re.search('\d',password)
 
 def get_password_strength(password):
-    pass
+    strength=0
+    if upper_case(password):
+        strength+=1
+    if lower_case(password):
+        strength+=1
+    if check_special_symbols(password):
+        strength+=1
+    return strength
 
 
 if __name__ == '__main__':
-    pass
+    print(get_password_strength("Pass"))
