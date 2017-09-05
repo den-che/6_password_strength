91d7ad2349a36cc2dffbeb1fa085ad9b6c7e6d72
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-09-06 00:12:48 +0300

    add in blacklist function

--- password_strength.py
+++ password_strength.py
@@ -1,14 +1,14 @@
 import re
 import os
 
-def load_password_blacklist(filepath):
-    
+def load_password_blacklist(filepath):  
     if not os.path.exists(filepath):
         return None
     else:
         with open(filepath,'r', encoding='utf-8') as file_blacklist:
             blacklist = file_blacklist.read().split("\n") 
             return blacklist
+
 
 def check_in_blacklist(password,filepath):
     blacklist = load_password_blacklist(filepath)
@@ -17,12 +17,14 @@ def check_in_blacklist(password,filepath):
     else:
         return False
 
+
 def check_numbers(password):
-    
     return re.findall('\d',password)
+
 
 def check_symbols(password):
     return re.findall('\W',password)
+
 
 def get_password_strength(password):
     strength=0
@@ -38,14 +40,14 @@ def get_password_strength(password):
     if check_symbols(password):
         strength+=3
         print("Password have special symbols")
-    if check_in_blacklist == False:
-        strength+=1
-        print("Password was not found in blacklist")
     return strength
 
 
 if __name__ == '__main__':
+    password = "vjht1051#"
     file_blacklist = "blacklist_password.txt"
     blacklist = list()
-    print(get_password_strength("vjht11"))
-    #print(load_password_blacklist(""))
+    if check_in_blacklist(password,file_blacklist) == False:
+        print(get_password_strength(password))
+    else:
+        print("Password was found in blacklist. Please, choose another password")
